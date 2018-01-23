436fb8dd85d201b4aa5f8c57eb66ac24dd3b9971
shinyatime <shinyatan@sina.com>
2018-01-23 15:46:36 +0800

    boot项目初始化

---
+++ src/main/java/cn/intan/thebooter/ThebooterApplication.java
@@ -0,0 +1,12 @@
+package cn.intan.thebooter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThebooterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThebooterApplication.class, args);
+	}
+}
